221d2e42b5883b75b1d018490a6f3446331c656f
Mariya Kazachkova <mariyakazachkova@Mariyas-MacBook-Pro.local>
2016-12-01 12:52:11 -0500

    Code to build fm index

---
+++ fm_build.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+import os
+from os.path import join, abspath, isfile, isdir, exists, basename
+import sys
+import fmindex
+
+def main():
+    if not len(sys.argv) in [3]:
+        print 'Usage: '
+        print '  %s data index' % sys.argv[0]
+        os.abort()
+    else:
+        if not isfile(sys.argv[1]):
+            print "Input file doesn't exist"
+            os.abort()
+        
+        inp = open(sys.argv[1])
+        
+        # read input
+        data = inp.read()
+        
+        # create index
+        idx = fmindex.index(data)
+        
+        # save index to file
+        fmindex.save(sys.argv[2], idx)
+
+        print "search results: "
+        list_matches = idx.search("ctaactgataccggaataccttaactacggacgtttgtttttgcgccacgc")
+        for each in list_matches:
+            print each
+
+
+if __name__ == '__main__':
+    main()
