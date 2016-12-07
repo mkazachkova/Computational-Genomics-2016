8d0d7aef5a609138429fe9a9d5eacaf4051b8a31
Mariya Kazachkova <mariyakazachkova@Mariyas-MacBook-Pro.local>
2016-12-07 16:40:29 -0500

    one and two mismatches works with naive and minimal testing

--- matches.py
+++ matches.py
@@ -4,16 +4,13 @@ from fm import FmIndex
 t = ""
 fmB = None
 
-def OneMismatch(p):
-	#p = "helloWorld"
+def oneMismatch(p):
 	global t
 	global fmB
-	occurrs = fmB.occurrences(p)
+	#occurrs = fmB.occurrences(p)
 
 	second = p[len(p)/2:]
 	first = p[:len(p)/2]
-	#print first
-	#print second
 
 	first_exact_matches = fmB.occurrences(first)
 	second_exact_matches = fmB.occurrences(second)
@@ -25,17 +22,73 @@ def OneMismatch(p):
 			start = index - len(first)
 			if (start >= 0):
 				#run boyer-more/naive starting at start and matching w/ up to one mismatch for first 
-				if naive(start, first):
+				bool_t, list_t = naive(start,first)	
+				if bool_t:
 					matches.append(start) #means there is a match or approximate match here; add to list
 
 	if first_exact_matches != None:
 		for index in first_exact_matches:
 			start = index + len(first) 
-			if (start +len(second)) <= (len(t)-1):
-				if naive(start,second):
+			if (start + len(second)) <= (len(t)-1):
+				bool_t, list_t = naive(start,second)
+				if bool_t:
 					matches.append(index)
+	print matches
 	return list(set(matches))
 
+def twoMismatch(p):
+	global t
+	global fmB
+
+	#p = "helloWorld"
+
+	matches = []
+
+	third = p[len(p)/3 + len(p)/3 :]
+	first = p[:len(p)/3]
+	second = p[len(p)/3:len(p)/3 + len(p)/3]
+
+	print first
+	print second
+	print third
+
+	first_exact_matches = fmB.occurrences(first)
+	second_exact_matches = fmB.occurrences(second)
+	third_exact_matches = fmB.occurrences(third)
+
+	for each in first_exact_matches:
+		if each + len(p) < len(t):
+			start_for_second = each + len(first)
+			start_for_third = start_for_second + len(second)
+			added1, mismatches1 = naive(start_for_second, second)
+			added2, mismatches2 = naive(start_for_third, third)
+			if added1 and added2:
+				if mismatches1 + mismatches2 < 3:
+					matches.append(each)
+
+	for index in second_exact_matches:
+		if (index - len(first)) >= 0 and (index + len(second) + len(first)) < len(t):
+			start_for_first = index - len(first)
+			start_for_third = index + len(second)
+			added1, mismatches1 = naive(start_for_first, first)
+			added2, mismatches2 = naive(start_for_third, third)
+			if added1 and added2:
+				if mismatches1 + mismatches2 < 3:
+					matches.append(start_for_first)
+
+	for index in third_exact_matches:
+		if (index - len(second) - len(first)) >= 0 and (index + len(third) < len(t)):
+			start_for_first = index - len(second) - len(first)
+			start_for_second = index - len(second)
+			added1, mismatches1 = naive(start_for_first, first)
+			added2, mismatches2 = naive(start_for_second, second)
+			if added1 and added2:
+				if mismatches1 + mismatches2 < 3:
+					matches.append(start_for_first)
+
+
+	print matches
+	return list(set(matches))
 
 def naive(start, compared): 
 	"""can be changed to boyer more (code available from piazza), but was thinking that since we 
@@ -46,9 +99,9 @@ def naive(start, compared):
 		if t[start] != compared[i]:
 			mismatches+=1
 		if mismatches > 1:
-			return False
+			return False, mismatches
 		start+=1
-	return True
+	return True, mismatches
 
 f = open("test_genome.txt", "r")
 t = f.read()
@@ -58,5 +111,11 @@ fmB = FmIndex(t)
 a = open("test_pattern_exact.txt", "r")
 p = a.read()
 
-matches = OneMismatch(p)
-print matches 
+#matches = OneMismatch(p)
+#print matches 
+
+
+listT = twoMismatch(p)
+#print listT
+for each in listT:
+	print t[each:each+len(p)]
